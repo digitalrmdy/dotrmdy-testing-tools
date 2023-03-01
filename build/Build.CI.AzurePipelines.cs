1cb374014aeb5e71005cb7db25620332a27c598a
Jens Schadron <jens.schadron@rmdy.be>
2023-03-01 13:14:14 +0100

    Add Publish pipeline

--- build/Build.CI.AzurePipelines.cs
+++ build/Build.CI.AzurePipelines.cs
@@ -10,6 +10,19 @@ using Nuke.Common.CI.AzurePipelines;
     InvokedTargets = new[] { nameof(Pack) },
     CacheKeyFiles = new string[0],
     CachePaths = new string[0])]
+[AzurePipelines(
+    suffix: "Publish",
+    AzurePipelinesImage.UbuntuLatest,
+    EnableAccessToken = true,
+    AutoGenerate = false,
+    FetchDepth = 0,
+    TriggerBatch = true,
+    TriggerTagsInclude = new[] { "'*.*.*'" },
+    ImportVariableGroups = new[] { "dotRMDY-MyGet" },
+    ImportSecrets = new[] { nameof(MyGetFeedUrl), nameof(MyGetApiKey) },
+    InvokedTargets = new[] { nameof(Publish) },
+    CacheKeyFiles = new string[0],
+    CachePaths = new string[0])]
 partial class Build
 {
 }
