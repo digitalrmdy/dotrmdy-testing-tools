22f4fc483eb267c1d6f5be9cda1ef35c165a1971
Maxim Storms <maxim@maximst.be>
2024-11-28 15:46:40 +0100

    Clean up

--- build/Build.CI.GithubActions.cs
+++ build/Build.CI.GithubActions.cs
@@ -1,5 +1,19 @@
 using Nuke.Common.CI.GitHubActions;
 
-[GitHubActions("pr", GitHubActionsImage.UbuntuLatest, On = [GitHubActionsTrigger.PullRequest], InvokedTargets = [nameof(Pack)], AutoGenerate = false, FetchDepth = 0, CacheKeyFiles = [])]
-[GitHubActions("publish", GitHubActionsImage.UbuntuLatest, InvokedTargets = [nameof(Publish)], AutoGenerate = false, FetchDepth = 0, CacheKeyFiles = [],OnPushTags = ["'*.*.*'"])]
+[GitHubActions(
+	"pr", 
+	GitHubActionsImage.UbuntuLatest, 
+	On = [GitHubActionsTrigger.PullRequest], 
+	InvokedTargets = [nameof(Pack)], 
+	AutoGenerate = false, 
+	FetchDepth = 0, 
+	CacheKeyFiles = [])]
+[GitHubActions(
+	"publish", 
+	GitHubActionsImage.UbuntuLatest, 
+	InvokedTargets = [nameof(Publish)], 
+	AutoGenerate = false, 
+	FetchDepth = 0, 
+	CacheKeyFiles = [],
+	OnPushTags = ["'*.*.*'"])]
 partial class Build;
